41d5add565b0b5e090f7ce2953b0df9c2975666b
Florian Zimmermann <flo@chaos-wg.net>
2020-01-12 18:45:47 +0100

    demonstrate the test conversion issue in Visual Studio 2015

---
+++ NunitTestAdapterIssue/Issue.cs
@@ -0,0 +1,15 @@
+﻿
+using NUnit.Framework;
+
+namespace NunitTestAdapterIssue
+{
+    [TestFixture]
+    public class Issue
+    {
+        [Test]
+        public void ShouldRun()
+        {
+            Assert.That(true, "this should run in VS 2015");
+        }
+    }
+}
